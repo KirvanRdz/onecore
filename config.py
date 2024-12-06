832c2bf11bfd7e7a38210231edbd938fe49f7e0d
KirvanRdz <kirvanrodriguez@hotmail.com>
2024-12-06 13:16:37 -0600

    Forzar recarga de variables de entorno con load_dotenv(override=True)

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 from datetime import timedelta
-
+from dotenv import load_dotenv
+load_dotenv(override=True) 
 class Config:
     SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15)
